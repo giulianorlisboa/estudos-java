8d64fd09c92c1fc5006418fd70f54105c25c8201
Giuliano <giuliano.lisboa.dev@gmail.com>
2026-07-23 20:00:59 -0300

    adiciona exercicio SomaVetores: soma elemento a elemento de dois vetores

---
+++ Vetores/SomaVetores/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Quantos valores vai ter cada vetor? ");
+        int n = sc.nextInt();
+        int[] vetA = new int[n];
+        int[] vetB = new int[n];
+        int[] vetC = new int[n];
+
+        System.out.println("Digite os valores do vetor A: ");
+        for (int i = 0; i < n; i++) {
+            vetA[i] = sc.nextInt();
+        }
+
+        System.out.println("Digite os valores do vetor B: ");
+        for (int i = 0; i < n; i++) {
+            vetB[i] = sc.nextInt();
+        }
+
+        System.out.println("VALOR RESULTANTE: ");
+        for (int i = 0; i < n; i++) {
+            vetC[i] = vetA[i] + vetB[i];
+            System.out.println(vetC[i]);
+        }
+
+        sc.close();
+    }
+}
