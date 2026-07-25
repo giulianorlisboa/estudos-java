982946c2efcc77497051da129557b70d8b9105a3
Giuliano <giuliano.lisboa.dev@gmail.com>
2026-07-25 13:49:02 -0300

    implementa repository e service de Cliente

---
+++ biblioteca-java/src/repository/ClienteRepository.java
@@ -0,0 +1,36 @@
+package repository;
+
+import model.Cliente;
+
+import java.util.ArrayList;
+
+public class ClienteRepository {
+    private ArrayList<Cliente> clientes = new ArrayList<>();
+    private int proximoId = 1;
+
+    public Cliente cadastrarCliente(String nome, int idade){
+        Cliente cliente = new Cliente(nome, proximoId, idade);
+        clientes.add(cliente);
+        return cliente;
+    }
+
+    public Cliente buscarCliente(int id){
+        return clientes.stream()
+                .filter(c -> c.getId() == id)
+                .findFirst()
+                .orElse(null);
+    }
+
+    public boolean removerCliente(int id){
+        return clientes.remove(buscarCliente(id));
+    }
+
+    public boolean atualizarNomeCliente(int id, String nome){
+        Cliente cliente = buscarCliente(id);
+        if (cliente == null){
+            return false;
+        }
+        cliente.setNome(nome);
+        return true;
+    }
+}
---
+++ biblioteca-java/src/service/ClienteService.java
@@ -0,0 +1,26 @@
+package service;
+
+import model.Cliente;
+import repository.ClienteRepository;
+
+public class ClienteService {
+    private ClienteRepository clienteRepository = new ClienteRepository();
+
+    public Cliente cadastrarCliente(String nome, int idade){
+        if (nome == null || nome.equals("")){
+            throw new IllegalArgumentException("O nome não pode estar em branco");
+        }
+        if (idade < 16 || idade > 100){
+            throw new IllegalArgumentException("O cliente deve ter 16 anos ou mais.");
+        }
+        return clienteRepository.cadastrarCliente(nome, idade);
+    }
+
+    public boolean removerCliente(int id){
+        return clienteRepository.removerCliente(id);
+    }
+
+    public boolean editarCliente(int idCliente, String nome){
+        return clienteRepository.atualizarNomeCliente(idCliente, nome);
+    }
+}
