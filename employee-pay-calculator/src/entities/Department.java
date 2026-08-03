33f68fe0b35096aa2f8d72dfcced9d1b261816c6
Giuliano <giuliano.lisboa.dev@gmail.com>
2026-08-03 20:40:13 -0300

    Corrige bug no construtor de Department, remove construtor incompleto de Worker e corrige ordem dos parâmetros em income()

--- employee-pay-calculator/src/entities/Department.java
+++ employee-pay-calculator/src/entities/Department.java
@@ -3,7 +3,9 @@ package entities;
 public class Department {
     private String name;
 
-    public Department(String name) {}
+    public Department(String name) {
+        this.name = name;
+    }
 
     public String getName() {
         return name;
--- employee-pay-calculator/src/entities/Worker.java
+++ employee-pay-calculator/src/entities/Worker.java
@@ -24,7 +24,6 @@ public class Worker {
         this.department = department;
     }
 
-
     public String getName() {
         return name;
     }
