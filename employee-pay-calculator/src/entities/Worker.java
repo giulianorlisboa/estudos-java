96c0e39daeb6d12b4a553ead0bb0a3ba2ec439de
Giuliano <giuliano.lisboa.dev@gmail.com>
2026-08-03 19:19:58 -0300

    adicionado construtores, getters e setters para a classe Worker

--- employee-pay-calculator/src/entities/Worker.java
+++ employee-pay-calculator/src/entities/Worker.java
@@ -2,6 +2,9 @@ package entities;
 
 import enums.WorkerLevel;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Worker {
     private String name;
     private WorkerLevel level;
@@ -9,5 +12,48 @@ public class Worker {
 
     private Department department;
 
-    public void addContra n
+    public List<HourContract> contracts = new ArrayList<>();
+
+    public Worker(){
+    };
+
+    public Worker(String name, WorkerLevel level, Double baseSalary, Department department) {
+        this.name = name;
+        this.level = level;
+        this.baseSalary = baseSalary;
+        this.department = department;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public WorkerLevel getLevel() {
+        return level;
+    }
+
+    public void setLevel(WorkerLevel level) {
+        this.level = level;
+    }
+
+    public Double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(Double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
 }
