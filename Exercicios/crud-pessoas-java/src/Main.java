7d072673ed76e155bb3d1570cbda9dedc390aab2
Giuliano <giuliano.lisboa.dev@gmail.com>
2026-08-25 19:01:12 -0300

    Criada logica do menu para cadastrar usuarios e listar os cadastrados

--- Exercicios/crud-pessoas-java/src/Main.java
+++ Exercicios/crud-pessoas-java/src/Main.java
@@ -1,3 +1,76 @@
-void main(){
+import entities.CadastroPessoas;
+import entities.Fisica;
+import entities.Juridica;
+import entities.Pessoa;
 
+void main() {
+    Scanner sc = new Scanner(System.in);
+    CadastroPessoas cp = new CadastroPessoas();
+
+    boolean rodando = true;
+    do {
+        IO.println("Digite a opção desejada: ");
+        IO.println("1 - Cadastro");
+        IO.println("2 - Listar cadastros");
+        IO.println("3 - Atualizar cadastro");
+        IO.println("4 - Deletar cadastro");
+        IO.println("0 - Sair");
+        int opcao = sc.nextInt();
+        sc.nextLine();
+        switch (opcao) {
+            case 1:
+                IO.print("Qual o tipo de pessoa que deseja cadastrar (F - física / J - juridica) ");
+                String tipo = sc.nextLine();
+                if (tipo.equalsIgnoreCase("F")) {
+                    IO.print("Digite o seu nome completo: ");
+                    String nomeCompleto = sc.nextLine();
+                    IO.print("Endereço: ");
+                    String endereco = sc.nextLine();
+                    IO.print("Telefone: ");
+                    Long telefone = sc.nextLong();
+                    sc.nextLine();
+                    IO.print("Email: ");
+                    String email = sc.nextLine();
+                    IO.print("CPF: ");
+                    String cpf = sc.nextLine();
+                    IO.print("Data de Nascimento: ");
+                    LocalDate dataNascimento = LocalDate.parse(sc.nextLine(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+
+                    Pessoa p = new Fisica(nomeCompleto, endereco, telefone, email, cpf, dataNascimento);
+                    cp.cadastrar(p);
+                    cp.exibirPessoas();
+
+                } else if (tipo.equalsIgnoreCase("j")) {
+                    IO.print("Digite o seu nome completo: ");
+                    String nomeCompleto = sc.nextLine();
+                    IO.print("Endereço: ");
+                    String endereco = sc.nextLine();
+                    IO.print("Telefone: ");
+                    Long telefone = sc.nextLong();
+                    sc.nextLine();
+                    IO.print("Email: ");
+                    String email = sc.nextLine();
+                    IO.print("CNPJ: ");
+                    String cpf = sc.nextLine();
+                    IO.print("Nome fantasia: ");
+                    String nomeFantasia = sc.nextLine();
+
+                    Pessoa p = new Juridica(nomeCompleto, endereco, telefone, email, cpf, nomeFantasia);
+                    cp.cadastrar(p);
+                    cp.exibirPessoas();
+
+
+                } else {
+                    IO.println("Opção inserida é inválida, tente novamente");
+                }
+                break;
+
+            case 2:
+                cp.exibirPessoas();
+                break;
+            case 0:
+                rodando = false;
+                break;
+        }
+    } while (rodando);
 }
