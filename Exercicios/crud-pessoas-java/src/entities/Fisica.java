91014d2c4ad9cc7d884687a6eb52b46dd5ae0973
Giuliano <giuliano.lisboa.dev@gmail.com>
2026-08-25 19:01:54 -0300

    Atualizada logica do ToString para exibir os dados

--- Exercicios/crud-pessoas-java/src/entities/Fisica.java
+++ Exercicios/crud-pessoas-java/src/entities/Fisica.java
@@ -1,16 +1,19 @@
 package entities;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Fisica extends Pessoa {
     private String cpf;
     private LocalDate dataNascimento;
 
+    final private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
     public Fisica() {
         super();
     }
 
-    public Fisica(String nome, String endereco, Integer telefone, String email, String cpf, LocalDate dataNascimento) {
+    public Fisica(String nome, String endereco, Long telefone, String email, String cpf, LocalDate dataNascimento) {
         super(nome, endereco, telefone, email);
         this.cpf = cpf;
         this.dataNascimento = dataNascimento;
@@ -34,12 +37,11 @@ public class Fisica extends Pessoa {
 
     @Override
     public String toString() {
-        IO.println("Nome: " + getNome());
-        IO.println("Endereco: " + getEndereco());
-        IO.println("Telefone: " + getTelefone());
-        IO.println("Email: " + getEmail());
-        IO.println("CPF: " + getCpf());
-        IO.println("Data de Nascimento: " + getDataNascimento());
-        return super.toString();
+        return "Nome: " + getNome()
+                + "\nEndereco: " + getEndereco()
+                + "\nTelefone: " + getTelefone()
+                + "\nEmail: " + getEmail()
+                + "\nCPF: " + getCpf()
+                + "\nData de Nascimento: " + getDataNascimento().format(formatter);
     }
 }
